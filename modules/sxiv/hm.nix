1aeccde8c6e02d71d58f08b11e8eb67171e1ee54
repparw <ubritos@gmail.com>
2026-09-08 14:12:53 -0300

    sxiv: gate auto-enable on x11 target

--- modules/sxiv/hm.nix
+++ modules/sxiv/hm.nix
@@ -1,18 +1,23 @@
-{ mkTarget, config, ... }:
-mkTarget {
-  autoEnable = config.stylix.targets.x11.enable;
-  autoEnableExpr = "config.stylix.targets.x11.enable";
+{ config, lib, ... }:
+let
+  cfg = config.stylix.targets.sxiv;
+  inherit (config.lib.stylix) colors;
+  inherit (config.stylix) fonts;
+in
+{
+  options.stylix.targets.sxiv.enable =
+    config.lib.stylix.mkEnableTargetWith {
+      name = "sxiv";
+      autoEnable = config.stylix.targets.x11.enable;
+      autoEnableExpr = "config.stylix.targets.x11.enable";
+    };
 
-  config = [
-    ({ fonts }: {
-      xresources.properties."Sxiv.font" =
+  config = lib.mkIf (config.stylix.enable && cfg.enable) {
+    xresources.properties = {
+      "Sxiv.font" =
         "${fonts.sansSerif.name}-${toString fonts.sizes.applications}";
-    })
-    ({ colors }: {
-      xresources.properties = {
-        "Sxiv.foreground" = "#${colors.base01}";
-        "Sxiv.background" = "#${colors.base04}";
-      };
-    })
-  ];
+      "Sxiv.foreground" = "#${colors.base01}";
+      "Sxiv.background" = "#${colors.base04}";
+    };
+  };
 }
