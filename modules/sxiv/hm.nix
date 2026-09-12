76d95786d50763b3c404fc2dded35f6f668dd87c
repparw <ubritos@gmail.com>
2026-09-12 19:36:20 -0300

    sxiv: format module and testbed

--- modules/sxiv/hm.nix
+++ modules/sxiv/hm.nix
@@ -5,17 +5,15 @@ let
   inherit (config.stylix) fonts;
 in
 {
-  options.stylix.targets.sxiv.enable =
-    config.lib.stylix.mkEnableTargetWith {
-      name = "sxiv";
-      autoEnable = config.stylix.targets.x11.enable;
-      autoEnableExpr = "config.stylix.targets.x11.enable";
-    };
+  options.stylix.targets.sxiv.enable = config.lib.stylix.mkEnableTargetWith {
+    name = "sxiv";
+    autoEnable = config.stylix.targets.x11.enable;
+    autoEnableExpr = "config.stylix.targets.x11.enable";
+  };
 
   config = lib.mkIf (config.stylix.enable && cfg.enable) {
     xresources.properties = {
-      "Sxiv.font" =
-        "${fonts.sansSerif.name}-${toString fonts.sizes.applications}";
+      "Sxiv.font" = "${fonts.sansSerif.name}-${toString fonts.sizes.applications}";
       "Sxiv.foreground" = "#${colors.base01}";
       "Sxiv.background" = "#${colors.base04}";
     };
